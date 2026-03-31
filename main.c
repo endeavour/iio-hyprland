f74e4fe1728e7c556e82b65d1a490c820bff5b6e
James Freiwirth <james@freiwirth.co.uk>
2026-03-31 17:31:30 +0100

    Rotating left and right now working

--- main.c
+++ main.c
@@ -161,9 +161,9 @@ void handle_orientation(enum Orientation orientation, const char* monitor_id) {
     }
     else {
         if (secondary != NULL) {
-            char cmd[512];
-            snprintf(cmd, sizeof(cmd), "hyprctl --batch \"keyword monitor %s,transform,%d ; keyword monitor %s,%dx%d@120,%dx%d,%d ; keyword input:touchdevice:transform %d ; keyword input:tablet:transform %d\"", 
-                output, orientation_transform, secondary, primary_width, primary_height, secondary_x, secondary_y, scale, orientation_transform, orientation_transform);
+            char cmd[1024];
+            snprintf(cmd, sizeof(cmd), "hyprctl --batch \"keyword monitor %s,transform,%d ; keyword monitor %s,%dx%d@120,%dx%d,%d ; keyword monitor %s,transform,%d ; keyword input:touchdevice:transform %d ; keyword input:tablet:transform %d\"", 
+                output, orientation_transform, secondary, primary_width, primary_height, secondary_x, secondary_y, scale, secondary, orientation_transform, orientation_transform, orientation_transform);
             printf("DEBUG: %s\n", cmd);
             system(cmd);
         } else {
